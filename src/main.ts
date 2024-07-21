2a63d98de02b606fabfa64da5288d96dad5eee60
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-07-21 09:06:34 +0300

    Use `getPopupEnh`

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
-import {makeMovableEx, makeResizableEx} from "./make-fancy.ts";
+import {getPopupEnh} from "./make-fancy.ts";
+
+const {makeMovableEx, makeResizableEx} = getPopupEnh("fancy-demo");
 
 const popup1 = document.querySelector<HTMLElement>("#popup-1")!;
 const popup1Header = popup1.querySelector<HTMLElement>(".popup-header")!;
-const {reset: resetPos1} =   makeMovableEx(popup1, "fancy-demo--popup-1",{ handle: popup1Header });
-const {reset: resetSiz1} = makeResizableEx(popup1, "fancy-demo--popup-1");
+const {reset: resetPos1} =   makeMovableEx(popup1, "popup-1",{ handle: popup1Header });
+const {reset: resetSiz1} = makeResizableEx(popup1, "popup-1");
 
 const popup2 = document.querySelector<HTMLElement>("#popup-2")!;
 const popup3 = document.querySelector<HTMLElement>("#popup-3")!;
 
-const {reset: resetPos2} = makeMovableEx(popup2, "fancy-demo--popup-2");
-const {reset: resetPos3} = makeMovableEx(popup3, "fancy-demo--popup-3");
+const {reset: resetPos2} = makeMovableEx(popup2, "popup-2");
+const {reset: resetPos3} = makeMovableEx(popup3, "popup-3");
 
 const resetBtn = document.querySelector("#reset-btn")!;
 resetBtn.addEventListener("click", () => {
--- src/make-fancy.ts
+++ src/make-fancy.ts
@@ -148,28 +148,25 @@ export function storeStateInLS<T extends AnyState, S extends string>(
     return {onMove, onStop: _onStop, state, reset};
 }
 
-export function makeMovableEx<S extends string>(
-    element: HTMLElement,
-    id: S extends "" ? never : S,
-    opt: MovableOpts = {}) {
-    return makeMovable(element, {
-        ...opt,
-        ...storeStateInLS({
-            id: `${id as string}--move-state`,
-            ...opt,
-        })
-    });
-}
-
-export function makeResizableEx<S extends string>(
-    element: HTMLElement,
-    id: S extends "" ? never : S,
-    opt: ResizableOpts = {}) {
-    return makeResizable(element, {
-        ...opt,
-        ...storeStateInLS({
-            id: `${id as string}--resize-state`,
-            ...opt,
-        })
-    });
+export function getPopupEnh<S extends string>(appName: S extends "" ? never : S) {
+    return {
+        makeMovableEx<S extends string>(element: HTMLElement, id: S extends "" ? never : S, opt: MovableOpts = {}) {
+            return makeMovable(element, {
+                ...opt,
+                ...storeStateInLS({
+                    id: `${appName as string}--${id as string}--move-state`,
+                    ...opt,
+                })
+            });
+        },
+        makeResizableEx<S extends string>(element: HTMLElement, id: S extends "" ? never : S, opt: ResizableOpts = {}) {
+            return makeResizable(element, {
+                ...opt,
+                ...storeStateInLS({
+                    id: `${appName as string}--${id as string}--resize-state`,
+                    ...opt,
+                })
+            });
+        },
+    }
 }
