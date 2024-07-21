11e0e22b71c936d87d960b5a0e5eb136bc2b417e
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-07-21 10:03:59 +0300

    Add "click" listener

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ const {makeMovableEx, makeResizableEx} = getPopupEnh("fancy-demo");
 
 const popup1 = document.querySelector<HTMLElement>("#popup-1")!;
 const popup1Header = popup1.querySelector<HTMLElement>(".popup-header")!;
-const {reset: resetPos1} =   makeMovableEx(popup1, "popup-1",{ handle: popup1Header });
+const {reset: resetPos1} =   makeMovableEx(popup1, "popup-1", {handle: popup1Header});
 const {reset: resetSiz1} = makeResizableEx(popup1, "popup-1");
 
 const popup2 = document.querySelector<HTMLElement>("#popup-2")!;
 const popup3 = document.querySelector<HTMLElement>("#popup-3")!;
 
 const {reset: resetPos2} = makeMovableEx(popup2, "popup-2");
-const {reset: resetPos3} = makeMovableEx(popup3, "popup-3");
+const {reset: resetPos3} = makeMovableEx(popup3, "popup-3", {
+    handle: popup3.querySelector<HTMLElement>(".popup-header")!
+});
 
 const resetBtn = document.querySelector("#reset-btn")!;
 resetBtn.addEventListener("click", () => {
@@ -19,4 +21,10 @@ resetBtn.addEventListener("click", () => {
     resetSiz1();
     resetPos2();
     resetPos3();
+});
+
+document.querySelectorAll<HTMLElement>(".number").forEach(elem => {
+    elem.addEventListener("click", _ => {
+        elem.innerText = String(Number(elem.innerText) + 1);
+    });
 });
