c4dea5c90f8b867bfc63c7f5af3e3da6286ef7ef
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-07-23 09:03:36 +0300

    Update texts

--- src-demo/init-html.ts
+++ src-demo/init-html.ts
@@ -2,12 +2,13 @@ document.querySelector(".popup-root")!.insertAdjacentHTML("afterbegin", `
       <div class="popup" id="popup-1">
         <div class="popup-header">Popup Title</div>
         <div class="popup-content">
-          <h2>Try it</h2>
-          <div>Some content.</div>
-          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Vel fringilla est ullamcorper eget. Egestas sed tempus urna et pharetra.
-            Aliquam etiam erat velit scelerisque in dictum non.
+          <h2>Try it move / resize it</h2>
+          <div>This popup is movable and resizable.</div>
+          <hr>
+          <div>You can move it by the drag handle — the title bar and resize it by the right bottom corner.</div>
+          <hr>
+          <div>The other two popups are only movable. 
+               The popup with the handle is interactable, you can click on the number to increase it.
           </div>
         </div>
       </div>
